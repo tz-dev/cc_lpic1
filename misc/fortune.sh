81ced7885c6292bae9de0bf006c1064a52d7b02f
tz <admin@noita-editor.com>
2020-06-21 15:24:25 +0200

    update fortune script and save a few dozen lines

--- misc/fortune.sh
+++ misc/fortune.sh
@@ -1,54 +1,25 @@
 #! /bin/bash
 clear													# clear screen
-
-(fortune -s > _fortune.txt)								# give wisdom & store in file
-sentence=$(<_fortune.txt)								# store content in variable
-
-let i=0													# index variable
-let j=0													# helper variable
-let pitch=50											# voice pitch
+sentence=$(fortune -s)									# store fortune in variable
 let chars=0												# variable for word character count
-let content=0											# variable for word content
-let column=0											# variable for cursor positioning
-let line=0												# variable for cursor positioning
+let column=0											# variable for cursor column positioning
+let line=0												# variable for cursor line positioning
 let width=$(tput cols)-30								# variable for console width & delimiting text
-
+let pitch=50											# voice pitch default
 for word in $sentence									# write words of sentence in single files
 do
-	echo $word > _fortune0$i
-	i=$[i+1]
-done
-
-while [ $i -gt 0 ]
-do
-	chars=$(wc -m < _fortune0$j)						# count characters in word for pause timing
-	content=$(<_fortune0$j)
-	column=$[column+chars]								# increment column for cursor positioning
-
-	if [[ $content == *"--"* ]]; then					# if end of citation is reached, lower voice pitch
-  		pitch=$[pitch-25]
-	fi
-
-	(cat _fortune0$j | espeak -a 75 -p $pitch -s 150 &)	# speak single word in background
-	cat _fortune0$j | pv -qL 15							# type word when spoken
-
-	if [ $column -gt $width ]; then						# switch to next line if max width is reached
+	chars=${#word}										# get length of word
+	column=$[column+chars+1]							# increment column for cursor positioning
+	if [ $column -gt $width ]; then						# jump to beginning of next line if max width is reached
 		column=0
 		line=$[line+1]
 	fi
-
-	tput cup $line $column								# position cursor before typing next word in sentence
-
-	if [ $chars -gt 10 ]								# longer pause if more than 9 characters occur in a single word
-	then
-		espeak -s 100 ...
-	else
-		espeak -s 100 ..
+	if [[ $word == *"--"* ]]; then						# if end of citation is reached, lower voice pitch
+  		pitch=$[pitch-25]
 	fi
-
-	i=$[i-1]
-	j=$[j+1]
+	(espeak -a 75 -p $pitch -s 150 $word&)				# speak word
+	echo $word | pv -qL 15								# print word with typewriter effect
+	espeak .. -s500										# insert pause between words
+	tput cup $line $column								# position cursor before typing next word in sentence
 done
-
-rm _fortune*											# remove obsolete files
 echo													# print blank line
