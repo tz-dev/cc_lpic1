c6607d0230af9762b674d5a6ea64f11928c4b0e9
tz <admin@noita-editor.com>
2020-06-21 15:27:22 +0200

    adjust comments in fortune script

--- misc/fortune.sh
+++ misc/fortune.sh
@@ -6,7 +6,7 @@ let column=0											# variable for cursor column positioning
 let line=0												# variable for cursor line positioning
 let width=$(tput cols)-30								# variable for console width & delimiting text
 let pitch=50											# voice pitch default
-for word in $sentence									# write words of sentence in single files
+for word in $sentence									# process each word of sentence
 do
 	chars=${#word}										# get length of word
 	column=$[column+chars+1]							# increment column for cursor positioning
@@ -17,7 +17,7 @@ do
 	if [[ $word == *"--"* ]]; then						# if end of citation is reached, lower voice pitch
   		pitch=$[pitch-25]
 	fi
-	(espeak -a 75 -p $pitch -s 150 $word&)				# speak word
+	(espeak -a 75 -p $pitch -s 150 $word&)				# speak word in background
 	echo $word | pv -qL 15								# print word with typewriter effect
 	espeak .. -s500										# insert pause between words
 	tput cup $line $column								# position cursor before typing next word in sentence
